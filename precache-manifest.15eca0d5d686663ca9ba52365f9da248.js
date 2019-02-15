7c9d3e84cb7bb8ab8c2e003c235e7767351033de
Zachary Ferreira <Pearlybrah@github.com>
2019-02-14 21:29:53 -0800

    Updates

---
+++ precache-manifest.15eca0d5d686663ca9ba52365f9da248.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0b547176d11b812969f",
+    "url": "/nfl-react/static/css/main.97a686e5.chunk.css"
+  },
+  {
+    "revision": "b0b547176d11b812969f",
+    "url": "/nfl-react/static/js/main.b0b54717.chunk.js"
+  },
+  {
+    "revision": "03c85120a2a145a5aa67",
+    "url": "/nfl-react/static/js/runtime~main.03c85120.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/nfl-react/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "cd83f26422d8a38abdff71f939530c18",
+    "url": "/nfl-react/index.html"
+  }
+];
